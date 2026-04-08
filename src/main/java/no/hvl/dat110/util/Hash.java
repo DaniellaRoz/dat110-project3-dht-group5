14c02001cd5bad6273d0df9a9a2ad7583dd0e943
Daniella Stensland <dellasten25@gmail.com>
2026-04-08 17:06:54 +0200

    task 1

--- src/main/java/no/hvl/dat110/util/Hash.java
+++ src/main/java/no/hvl/dat110/util/Hash.java
@@ -6,61 +6,48 @@ package no.hvl.dat110.util;
  *
  */
 
-import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-public class Hash { 
-	
-	
-	public static BigInteger hashOf(String entity) {	
-		
-		BigInteger hashint = null;
-		
-		// Task: Hash a given string using MD5 and return the result as a BigInteger.
-		
-		// we use MD5 with 128 bits digest
-		
-		// compute the hash of the input 'entity'
-		
-		// convert the hash into hex format
-		
-		// convert the hex into BigInteger
-		
-		// return the BigInteger
-		
-		return hashint;
-	}
-	
-	public static BigInteger addressSize() {
-		
-		// Task: compute the address size of MD5
-		
-		// compute the number of bits = bitSize()
-		
-		// compute the address size = 2 ^ number of bits
-		
-		// return the address size
-		
-		return null;
-	}
-	
-	public static int bitSize() {
-		
-		int digestlen = 0;
-		
-		// find the digest length
-		
-		return digestlen*8;
-	}
-	
-	public static String toHex(byte[] digest) {
-		StringBuilder strbuilder = new StringBuilder();
-		for(byte b : digest) {
-			strbuilder.append(String.format("%02x", b&0xff));
-		}
-		return strbuilder.toString();
-	}
+public class Hash {
+    public static BigInteger hashOf(String entity) {    
+        BigInteger hashint = null;
+        
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
 
+            String entityHex = toHex(md.digest(entity.getBytes()));
+
+            hashint = new BigInteger(entityHex, 16);
+        } catch (NoSuchAlgorithmException err) {
+            System.err.println(err);
+        }
+        
+        return hashint;
+    }
+    
+    public static BigInteger addressSize() {
+        return BigInteger.TWO.pow(bitSize());
+    }
+    
+    public static int bitSize() {
+        int digestlen = 0;
+        
+        try {
+            digestlen = MessageDigest.getInstance("MD5").getDigestLength();
+        } catch (NoSuchAlgorithmException err) {
+            System.err.println(err);
+        }
+
+        return digestlen*8;
+    }
+    
+    public static String toHex(byte[] digest) {
+        StringBuilder strbuilder = new StringBuilder();
+        for(byte b : digest) {
+            strbuilder.append(String.format("%02x", b&0xff));
+        }
+        return strbuilder.toString();
+    }
 }
